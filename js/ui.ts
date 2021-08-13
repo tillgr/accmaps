8dd5a417a2e3929fdd1f550d0f8a50ed3aa0253a
Fabian <fabian.lueders@mailbox.tu-dresden.de>
2021-08-13 09:15:33 +0200

    add user profile selection dialog (WIP)

--- js/ui.ts
+++ js/ui.ts
@@ -8,20 +8,27 @@ import {BuildingControl} from "./buildingControl";
 import "materialize-css/dist/css/materialize.css";
 import "leaflet/dist/leaflet.css";
 import "../css/style.css";
+import {UserProfile} from "./userProfile";
+import {UserGroup} from "./data/userGroup";
 
 
 document.addEventListener('DOMContentLoaded', function () {
     initMaterialize();
     handleSearchForm();
+    selectUserprofile();
 });
 
 function initMaterialize() {
-    const modal = document.querySelectorAll('.modal');
-    Modal.init(modal, {
+    const searchModal = document.getElementById('buildingSearchModal');
+    const userProfileModal = document.getElementById('userProfileModal');
+
+    Modal.init(searchModal, {
         onOpenEnd: () => {
             document.getElementById('buildingSearch').focus();
         }
     });
+
+    Modal.init(userProfileModal, {}).open();
 }
 
 function handleSearchForm() {
@@ -80,4 +87,16 @@ function showSearchErrorMsg(errorMessage: string) {
         buildSearchErrorMsgContainer.classList.remove('scale-in');
         buildSearchErrorMsgContainer.classList.add('scale-out');
     }, 5000)
+}
+
+function selectUserprofile(){
+    document.getElementById('userProfileNormal').addEventListener('click', ()=> setUserprofile(UserGroup.noImpairments));
+    document.getElementById('userProfileVI').addEventListener('click', ()=> setUserprofile(UserGroup.blindPeople));
+    document.getElementById('userProfileWheelchair').addEventListener('click', ()=> setUserprofile(UserGroup.blindPeople));
+}
+
+function setUserprofile(userGroup: UserGroup){
+    UserProfile.set(userGroup);
+    const modal = document.getElementById('userProfileModal');
+    Modal.getInstance(modal).close();
 }
--- js/userProfile/index.ts
+++ js/userProfile/index.ts
@@ -1,3 +1,13 @@
 import {UserGroup} from "../data/userGroup";
 
 let selectedUserGroup: UserGroup = UserGroup.noImpairments;
+
+
+export const UserProfile = {
+    get() {
+        return selectedUserGroup;
+    },
+    set(userGroup: UserGroup) {
+        selectedUserGroup = userGroup;
+    }
+}
