4b8d71e621472b5b1525866baae305ae5406f94b
Fabian <fabian.lueders@mailbox.tu-dresden.de>
2021-09-27 10:59:27 +0200

    make features not clickable in screenreader-mode

--- src/indoorLayer/_featureScreenAccessibility.ts
+++ src/indoorLayer/_featureScreenAccessibility.ts
@@ -1,6 +1,6 @@
 export function featureScreenAccessibility(): void {
     const featurePaths = document.getElementsByClassName('leaflet-interactive');
     for (let i = 0; i < featurePaths.length; i++) {
-        featurePaths[i].setAttribute('role', 'button');
+        featurePaths[i].setAttribute('aria-disabled', 'true');
     }
 }
