554cac386b5e3bc75d628aaaa364ab2b5b548273
Fabian <fabian.lueders@mailbox.tu-dresden.de>
2021-09-27 09:51:57 +0200

    reload window after profile change to properly update all information

--- src/userProfile/index.ts
+++ src/userProfile/index.ts
@@ -1,5 +1,4 @@
 import {UserGroupEnum} from "../interfaces/userGroupEnum";
-import {LevelAccessibilityInformation} from "../levelControl/_levelAccessibilityInformation";
 
 let selectedUserGroup: UserGroupEnum = <UserGroupEnum>parseInt(localStorage.getItem('userProfile')) ?? UserGroupEnum.noImpairments;
 
@@ -9,6 +8,7 @@ export const UserProfile = {
     },
     set(userGroup: UserGroupEnum): void {
         selectedUserGroup = userGroup;
-        LevelAccessibilityInformation.reset();
+
+        setTimeout(window.location.reload.bind(window.location), 200);
     }
 }
