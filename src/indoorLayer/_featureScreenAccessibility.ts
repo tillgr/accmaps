df35e4ca0d134ff5db597379cb34da6f4c9e994a
Fabian <fabian.lueders@mailbox.tu-dresden.de>
2021-10-04 18:08:51 +0200

    make marker icons unfocusable on SR devices #2

--- src/indoorLayer/_featureScreenAccessibility.ts
+++ src/indoorLayer/_featureScreenAccessibility.ts
@@ -3,4 +3,10 @@ export function featureScreenAccessibility(): void {
     for (let i = 0; i < featurePaths.length; i++) {
         featurePaths[i].setAttribute('aria-disabled', 'true');
     }
+
+    const markerIcons = document.getElementsByClassName('leaflet-marker-icon');
+    for (let i = 0; i < markerIcons.length; i++) {
+        markerIcons[i].setAttribute('aria-disabled', 'true');
+        markerIcons[i].removeAttribute('tabindex');
+    }
 }
