8d3ed6bfcc53578483904a66ef17211ccb449f42
Fabian <fabian.lueders@mailbox.tu-dresden.de>
2021-10-08 14:54:26 +0200

    some style adjustments

--- webpack.config.js
+++ webpack.config.js
@@ -17,17 +17,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(scss)$/,
-                use: [{loader: 'style-loader'}, {loader: 'css-loader'},
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: function () {
-                                    return [require('autoprefixer')];
-                                }
-                            }
-                        }
-                    }, {loader: 'sass-loader'}]
+                use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'sass-loader'}]
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
