ae5073b03db7f0fd7c08aec50d0415aa72ef110e
Fabian <fabian.lueders@mailbox.tu-dresden.de>
2021-12-07 16:23:13 +0100

    typos
    (cherry picked from commit 906facdb56d10d98d6fa2b8aec6f463de86b4168)

--- src/data/featureAccessibilityProperties.ts
+++ src/data/featureAccessibilityProperties.ts
@@ -7,75 +7,105 @@ const allGroups = [
   UserGroupEnum.wheelchairUsers,
 ];
 
-export const featureAccessibilityProperties: AccessibilityPropertiesInterface[] =
-  [
+/**
+ * List of all possible accessibility features, which can be shown on click or by indication icons.
+ * Currently, only the very FIRST hit in this list is shown.
+ * So the most specific properties should be listed first, afterwards the more general ones.
+ */
+export const featureAccessibilityProperties: AccessibilityPropertiesInterface[] = [
+
+    /* ================ blind people ================ */
+    /* tactile information board / tactile map */
     {
-      accessibilityFunction: (f) => f.properties.handrail !== undefined,
-      msgTrue: "handrail available",
-      msgFalse: "no handrail available",
-      userGroups: [UserGroupEnum.blindPeople],
-      iconFilename: "handrail.png",
+        accessibilityFunction: (f) => ((f.properties.information !== undefined && ['tactile_map', 'tactile_model', 'braille', 'tactile_letters'].includes(f.properties.information))),
+        msgTrue: 'tactile information board / tactile map',
+        msgFalse: null,
+        userGroups: [UserGroupEnum.blindPeople],
+        iconFilename: 'information.svg'
     },
     {
-      accessibilityFunction: (f) => f.properties.tactile_paving !== undefined,
-      msgTrue: "tactile paving available",
-      msgFalse: "no tactile paving available",
-      userGroups: [UserGroupEnum.blindPeople],
-      iconFilename: "tactile_paving.png",
+        accessibilityFunction: (f) => f.properties['speech_output:de'] !== undefined || f.properties['speech_output:en'] !== undefined || f.properties['speech_output'] !== undefined,
+        msgTrue: 'speech output available',
+        msgFalse: null,
+        userGroups: [UserGroupEnum.blindPeople]
+    },
+
+    /* ================ wheelchair users ================ */
+    {
+        accessibilityFunction: (f) => (f.properties.amenity !== undefined && f.properties.amenity === 'toilets' && f.properties.wheelchair !== undefined && ['yes', 'designated'].includes(f.properties.wheelchair)),
+        msgTrue: 'accessible toilet',
+        msgFalse: null,
+        userGroups: [UserGroupEnum.wheelchairUsers],
+        iconFilename: 'toilets_wheelchair.svg'
     },
     {
-      accessibilityFunction: (f) =>
-        f.properties.amenity !== undefined &&
-        f.properties.amenity === "toilets",
-      msgTrue: (f) =>
-        (f.properties.male !== undefined
-          ? "male "
-          : f.properties.female !== undefined
-          ? "female "
-          : "unisex ") + "toilet",
-      msgFalse: null,
-      userGroups: allGroups,
+        accessibilityFunction: (f) => (f.properties.highway !== undefined && f.properties.highway === 'elevator' && f.properties.wheelchair !== undefined && ['yes', 'designated'].includes(f.properties.wheelchair)),
+        msgTrue: 'elevator',
+        msgFalse: null,
+        userGroups: [UserGroupEnum.wheelchairUsers],
+        iconFilename: 'elevator2_freepik.svg'
     },
     {
-      accessibilityFunction: (f) =>
-        f.properties.wheelchair !== undefined &&
-        ["yes", "designated"].includes(f.properties.wheelchair),
-      msgTrue: "wheelchair access possible",
-      msgFalse: "no wheelchair access",
-      userGroups: [UserGroupEnum.wheelchairUsers],
-      iconFilename: "wheelchair.png",
+        accessibilityFunction: (f) => (f.properties.wheelchair !== undefined && ['yes', 'designated'].includes(f.properties.wheelchair)),
+        msgTrue: 'wheelchair access possible',
+        msgFalse: 'no wheelchair access',
+        userGroups: [UserGroupEnum.wheelchairUsers],
+        iconFilename: 'wheelchair.png'
     },
     {
-      accessibilityFunction: (f) =>
-        f.properties["wheelchair:description:en"] !== undefined,
-      msgTrue: (f) => f.properties["wheelchair:description:en"],
-      msgFalse: null,
-      userGroups: [UserGroupEnum.wheelchairUsers],
+        accessibilityFunction: (f) => (f.properties['wheelchair:description:en'] !== undefined),
+        msgTrue: (f) => f.properties['wheelchair:description:en'],
+        msgFalse: null,
+        userGroups: [UserGroupEnum.wheelchairUsers]
     },
     {
-      accessibilityFunction: (f) =>
-        f.properties["wheelchair:description:de"] !== undefined,
-      msgTrue: (f) => f.properties["wheelchair:description:de"],
-      msgFalse: null,
-      userGroups: [UserGroupEnum.wheelchairUsers],
+        accessibilityFunction: (f) => (f.properties['wheelchair:description:de'] !== undefined),
+        msgTrue: (f) => f.properties['wheelchair:description:de'],
+        msgFalse: null,
+        userGroups: [UserGroupEnum.wheelchairUsers]
     },
+
+    /* ================ general features ================ */
+    /* toilets - general (no wheelchair) */
     {
-      accessibilityFunction: (f) =>
-        f.properties["speech_output:de"] !== undefined ||
-        f.properties["speech_output:en"] !== undefined ||
-        f.properties["speech_output"] !== undefined,
-      msgTrue: "speech output available",
-      msgFalse: null,
-      userGroups: [UserGroupEnum.blindPeople],
+        accessibilityFunction: (f) => (f.properties.amenity !== undefined && f.properties.amenity === 'toilets' && (f.properties.wheelchair === undefined || !['yes', 'designated'].includes(f.properties.wheelchair))),
+        msgTrue: (f) => ((f.properties.male !== undefined) ? 'male ' : (f.properties.female !== undefined) ? 'female ' : 'unisex ') + 'toilet',
+        msgFalse: null,
+        userGroups: [UserGroupEnum.noImpairments, UserGroupEnum.blindPeople],
+        iconFilename: 'toilets.svg'
     },
+    /* entrances (general) */
     {
-      accessibilityFunction: (f) =>
-        f.properties["information"] !== undefined &&
-        (f.properties["information"] === "braille" ||
-          f.properties["information"] === "tactile_letters"),
-      msgTrue: "tactile information available",
-      msgFalse: null,
-      userGroups: [UserGroupEnum.blindPeople],
-      iconFilename: "braille.png",
+        accessibilityFunction: (f) => (f.properties.entrance !== undefined && ['yes', 'main', 'secondary'].includes(f.properties.entrance)),
+        msgTrue: (f) => ((f.properties.entrance === 'main') ? 'main ' : (f.properties.entrance === 'secondary') ? 'secondary ' : '') + 'entrance',
+        msgFalse: null,
+        userGroups: allGroups,
+        iconFilename: 'entrance.svg'
     },
-  ];
+    /* emergency exist (general) */
+    {
+        accessibilityFunction: (f) => ((f.properties.exit !== undefined && ['yes', 'emergency'].includes(f.properties.exit)) ||
+            (f.properties.entrance !== undefined && ['exit', 'emergency'].includes(f.properties.entrance))),
+        msgTrue: 'exit',
+        msgFalse: null,
+        userGroups: allGroups,
+        iconFilename: 'emergency_exit.svg'
+    },
+    /* information boards (general, except blind people) */
+    {
+        accessibilityFunction: (f) => ((f.properties.information !== undefined && ['board', 'map'].includes(f.properties.information))),
+        msgTrue: 'information board / map',
+        msgFalse: null,
+        userGroups: [UserGroupEnum.noImpairments, UserGroupEnum.wheelchairUsers],
+        iconFilename: 'information.svg'
+    },
+
+    /* stairs (general, except wheelchair users) */
+    {
+        accessibilityFunction: (f) => ((f.properties.highway !== undefined && f.properties.highway === 'steps') || (f.properties.stairs !== undefined && f.properties.stairs === 'yes')),
+        msgTrue: 'stairs',
+        msgFalse: null,
+        userGroups: [UserGroupEnum.noImpairments, UserGroupEnum.blindPeople],
+        iconFilename: 'stairs.svg'
+    },
+];
