aca763c9f620495d99ab3f5ccec17c8e34c8a3ad
Till <till2000@live.com>
2021-12-09 20:14:48 +0100

    move getLevelsFromLevelString

--- src/services/levelService/levelService.ts
+++ src/services/levelService/levelService.ts
@@ -5,7 +5,6 @@ import { LevelAccessibilityInformation } from "./_levelAccessibilityInformation"
 import { DescriptionArea } from "../../components/ui/descriptionArea";
 
 import { INDOOR_LEVEL } from "../data/constants";
-import { getLevelsFromLevelString } from "./_getLevelsFromLevelString";
 import { createLevelControlButtons } from "./_createLevelControlButtons";
 import { BuildingService } from "../buildingService";
 
@@ -67,6 +66,55 @@ export const LevelService = {
   },
 };
 
+export function getLevels(level: string): string[] {
+  const finalArray = [];
+
+  let dashCount = 0;
+  let minLevel = 0;
+  let maxLevel = 0;
+
+  let firstDash = -1;
+  let secondDash = -1;
+  let thirdDash = -1;
+
+  for (let i = 0; i < level.length; i++) {
+    if (level.charAt(i) === "-") {
+      dashCount++;
+      if (firstDash === -1) {
+        firstDash = i;
+      } else if (secondDash === -1) {
+        secondDash = i;
+      } else if (thirdDash === -1) {
+        thirdDash = i;
+      }
+    }
+  }
+  if (dashCount === 1 && firstDash !== 0) {
+    // [0-5] but not [-5]
+    minLevel = parseInt(level.slice(0, firstDash));
+    maxLevel = parseInt(level.slice(firstDash + 1, level.length));
+  } else if (dashCount === 2) {
+    // [-1-5]
+    minLevel = parseInt(level.slice(0, secondDash));
+    maxLevel = parseInt(level.slice(secondDash + 1, level.length));
+  } else if (dashCount === 3) {
+    // [-1--5] or [-5--1]
+    minLevel = parseInt(level.slice(0, secondDash));
+    maxLevel = parseInt(level.slice(thirdDash, level.length));
+
+    if (minLevel > maxLevel) {
+      [minLevel, maxLevel] = [maxLevel, minLevel];
+    }
+  }
+  if (level.charAt(0) !== "-" || dashCount > 1) {
+    // not [-5]
+    for (let i = minLevel; i <= maxLevel; i++) {
+      finalArray.push(i.toString());
+    }
+  }
+  return finalArray;
+}
+
 function _getAllLevelNamesFromGeoJSON(): void {
   currentBuildingIndoorData.features.map(
     (feature: GeoJSON.Feature<any, any>) => {
@@ -80,7 +128,7 @@ function _getAllLevelNamesFromGeoJSON(): void {
       if (level.includes(";")) {
         feature.properties.level = level.split(";");
       } else if (level.includes("-")) {
-        feature.properties.level = getLevelsFromLevelString(level);
+        feature.properties.level = getLevels(level);
       }
 
       if (Array.isArray(feature.properties.level)) {
