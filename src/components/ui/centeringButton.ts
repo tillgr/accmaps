62d3b86bf413b13b13a76a64433e3793b032a8e3
Till <till2000@live.com>
2022-03-24 23:31:04 +0100

    fix map buttons and navbar

--- src/components/ui/centeringButton.ts
+++ src/components/ui/centeringButton.ts
@@ -3,9 +3,13 @@ import { geoMap } from "../../main";
 const btn = document.getElementById("centeringButton");
 
 function create(): void {
-    btn.addEventListener("click", geoMap.centerMapToBuilding);
+  btn.addEventListener("click", geoMap.centerMapToBuilding);
+
+  const container = document.querySelector(".leaflet-top.leaflet-right");
+
+  container.append(btn);
 }
 
 export default {
-    create
+  create,
 };
