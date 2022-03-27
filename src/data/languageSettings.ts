5ff7aee5d01e180386a637b6af0fa4a1d5ff3623
Adrian Köhler <adrian.koehler@mailbox.tu-dresden.de>
2022-03-27 20:12:45 +0200

    HTML "lang" attribute now changes according to selected language

--- src/data/languageSettings.ts
+++ src/data/languageSettings.ts
@@ -3,10 +3,12 @@ import { LanguageSettingsEnum } from "../models/languageSettingsEnum";
 const LanguageSettings: Map<LanguageSettingsEnum, any> = new Map<LanguageSettingsEnum, any>();
 LanguageSettings.set(LanguageSettingsEnum.english, {
   name: "English",
+  acronym: "en",
   resourceFile: "../../public/strings/lang.en.json"
 });
 LanguageSettings.set(LanguageSettingsEnum.german, {
   name: "Deutsch",
+  acronym: "de",
   resourceFile: "../../public/strings/lang.de.json"
 });
 
