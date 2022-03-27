696b38cfe237cc402d1468ee2d047a64b3d58b51
Adrian Köhler <adrian.koehler@mailbox.tu-dresden.de>
2022-03-27 23:53:23 +0200

    Fix: Room numbers appear after level change by keyboard

--- src/components/ui/levelControl.ts
+++ src/components/ui/levelControl.ts
@@ -51,6 +51,7 @@ function render(allLevelNames: string[]): void {
     levelBtn.addEventListener("keyup", (e: KeyboardEvent) => {
       if (e.key === "Enter") {
         geoMap.handleLevelChange(level);
+        geoMap.updateRoomLabels(); //makes sure room numbers don't appear after level change
 
         for (let i = 0; i < levelControl.children.length; i++) {
           levelControl.children[i].classList.remove("active");
