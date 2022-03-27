23a1844e4a7c53b6cb2d9543e57bfc431cf7e097
tomlud <tomlud111@gmail.com>
2022-03-27 19:10:47 +0200

    add aria-hidden to centering button icon

--- src/components/ui/centeringButton.ts
+++ src/components/ui/centeringButton.ts
@@ -4,7 +4,7 @@ function create(): void {
   const btnString = `<button type="button" class="btn btn-light d-flex p-1 shadow leaflet-control" 
                              aria-label="Set view to the focused building"
                              id="centeringButton">
-                        <span class="material-icons" role="presentation">center_focus_weak</span>
+                        <span class="material-icons" role="presentation" aria-hidden="true">center_focus_weak</span>
                      </button>`;
 
   const container = document.querySelector(".leaflet-top.leaflet-right");
